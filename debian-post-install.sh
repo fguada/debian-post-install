a17736f8881e08f6b02941c5002fc575276dc1f0
Franck Guadagnini <ipse@franckguadagnini.fr>
2025-03-12 12:20:14 +0100

    big refactor

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -1,34 +1,289 @@
 #!/bin/sh
 
-# À faire *avant* l'exécution de ce script:
+# set -o errexit
+# set -o nounset
+# set -o noglob # "Disable pathname expansion"
+# exec 2>|"$HOME/log.txt" # pour envoyer la sortie de xtrace dans un fichier
+# set -o xtrace
+# IFS="
+# "
+# IFS="$(printf '\n\t')" # par défaut: ' \n\t'
+
+# À faire AVANT l'exécution de ce script:
 # sudo apt install localepurge
 # sudo apt install git curl
-# git clone fguada…
+# git clone https://github.com/fguada/debian-post-install
+# cd ./debian-post-install
+# chmod +x ./debian-post-install.sh
+# sh ./debian-post-install.sh
 
-# Debian Linux Post-Installation Script for Wayland
+bold=$(tput bold)
+reset=$(tput sgr0)
 
-# Enable debugging output and exit on error
-# set -x
+echo '##########################################'
+echo "${bold}# SCRIPT DE POST-INSTALLATION DE DEBIAN. #${reset}"
+echo '##########################################'
 
-# Add user to sudo group
-# sudo usermod -aG sudo "$USER"
+echo
+echo "${bold}Configuration interactive de la console.${reset}"
+echo
 
-# Backup the existing sources.list file
+sudo dpkg-reconfigure console-setup
+
+echo
+echo "${bold}Création d’une copie de sauvegarde du fichier « /etc/apt/sources.list ».${reset}"
+echo
+
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
-# Add contrib and non-free to the sources.list
-sudo sed --in-place '/^deb .* main/{s/main/main contrib non-free/;t;}' /etc/apt/sources.list
-sudo sed --in-place 's/^#deb/deb/g}' /etc/apt/sources.list
+echo
+echo "${bold}Ajout des composants « contrib » et « non-free » au même fichier.${reset}"
+echo
 
-# On change légèrement les couleurs de apt.
-sudo touch "/etc/apt/apt.conf.d/21-colors.conf"
-sudo echo 'APT::Color::Action::Upgrade "blue";' >|"/etc/apt/apt.conf.d/21-colors.conf"
+sudo sed --in-place 's/main non-free-firmware/main non-free-firmware contrib non-free/g' /etc/apt/sources.list
 
-# Update package lists and upgrade existing packages
+echo
+echo "${bold}Configuration des couleurs d’apt.${reset}"
+echo
+
+apt_conf_dir='/etc/apt/apt.conf.d'
+apt_color_conf='21-colors.conf'
+test ! -d "$apt_conf_dir" && sudo mkdir --parents "$apt_conf_dir"
+sudo touch "$apt_conf_dir/$apt_color_conf"
+sudo echo 'APT::Color::Action::Upgrade "blue";' >|"$apt_conf_dir/$apt_color_conf"
+
+echo
+echo "${bold}Mise à jour de la liste des paquets, et installation d’éventuelles mises à jour de ceux-ci.${reset}"
+echo
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 alias install='sudo apt install -y'
+
+echo
+echo "${bold}Installation des paquets.${reset}"
+echo
+
+pkgs="\
+alacritty \
+arc \
+arj \
+atool \
+bash-completion \
+bat \
+bc \
+bfs \
+blueman \
+bluez \
+bluez-tools \
+brightnessctl \
+bsdutils \
+bzip2 \
+calibre \
+cargo \
+catdoc \
+chafa \
+chromium \
+chromium-l10n \
+clinfo \
+cliphist \
+cmake \
+colordiff \
+colortest-python \
+cpio \
+curl \
+dbus \
+dconf-editor \
+dex \
+dictionaries-common \
+diffutils \
+diskus \
+dmidecode \
+docx2txt \
+dra \
+du-dust \
+duf \
+eject \
+e2fsprogs \
+exfatprogs \
+exif \
+eza \
+fastfetch \
+fd-find \
+fdisk \
+ffmpegthumbnailer \
+firefox-esr \
+firefox-esr-l10n-fr \
+foot \
+fzf \
+galternatives \
+gcal \
+gh \
+gnome-characters \
+gnome-epub-thumbnailer \
+golang \
+gparted \
+grim \
+gucharmap \
+gvfs \
+gvfs-backends \
+gvfs-fuse \
+gzip \
+htop \
+hunspell \
+hyperfine \
+hyphen-fr \
+imagemagick \
+info \
+inxi \
+jekyll \
+jq \
+kanshi \
+keepassxc \
+kid3-qt \
+kitty \
+kitty-terminfo \
+labwc \
+lf \
+libcdio-utils \
+libcpanel-json-xs-perl \
+libdvd-pkg \
+libfuse2t64 \
+libglib2.0-bin \
+libnotify-bin \
+libnotify-dev \
+libreoffice-calc \
+libreoffice-grammalecte \
+libreoffice-help-fr \
+libreoffice-java-common \
+libreoffice-l10n-fr \
+libreoffice-qt6 \
+libreoffice-writer \
+libxml-dumper-perl \
+localepurge \
+lxpolkit \
+lzip \
+lzop \
+mako-notifier \
+mediainfo \
+mesa-utils \
+meson \
+micro \
+mintstick \
+moreutils \
+mousepad \
+mpv \
+neovim \
+network-manager \
+nomarch \
+odt2txt \
+pandoc \
+parallel \
+pdfarranger \
+plocate \
+poppler-utils \
+qbittorrent \
+qimgv \
+qt5ct \
+qt6ct \
+rar \
+rclone \
+regionset \
+rfkill \
+ripgrep \
+rpm \
+rsync \
+ruby-jekyll-paginate \
+ruby-jekyll-sitemap \
+screenruler \
+sd \
+sensible-utils \
+shellcheck \
+shfmt \
+slurp \
+swayidle \
+tar \
+tealdeer \
+thunar \
+thunar-archive-plugin \
+thunar-gtkhash \
+thunar-media-tags-plugin \
+thunar-volman \
+thunderbird \
+thunderbird-l10n-fr \
+timeshift \
+transmission-cli \
+trash-cli \
+tree \
+udisks2 \
+unace \
+unalz \
+unrar \
+upower \
+vainfo \
+vlc \
+vlc-plugin-pipewire \
+vulkan-tools \
+wayland-utils \
+wev \
+wget \
+wl-clipboard \
+wlopm \
+wlr-randr \
+wlrctl \
+wmctrl \
+xz-utils \
+yt-dlp \
+zathura"
+# abcde \
+# archivemount \
+# atril \
+# catfish \
+# featherpad \
+# network-manager-gnome \
+# numlockx \
+# parted \
+# partitionmanager \
+# pavucontrol-qt \
+# pavucontrol-qt-l10n \
+# qpdf \
+# quodlibet \
+# sxhkd \
+# synaptic \
+# syncthing \
+# transmission-qt \
+# xarchiver \
+# xcalib \
+# xcape \
+# xclip \
+# xdotool \
+# xfce4 \
+# xfce4-appfinder \
+# xfce4-notifyd \
+# xfce4-panel \
+# xfce4-power-manager \
+# xfce4-pulseaudio-plugin \
+# xfce4-screenshooter \
+# xfce4-session \
+# xfce4-settings \
+# xfce4-terminal \
+# xfce4-xkb-plugin \
+# xfconf \
+# xfwm4 \
+
+# shellcheck disable=SC2086 # On veut séparer les paquets.
+install $pkgs
+
+echo
+echo "${bold}Configuration de libdvd.${reset}"
+echo
+
+sudo dpkg-reconfigure libdvd-pkg
+
+echo
+echo "${bold}Création de répertoires nécessaires.${reset}"
+echo
 
 userdirs="\
 $HOME/Projets
@@ -48,10 +303,26 @@ for dir in $systemdirs; do
   sudo mkdir --parents "$dir"
 done
 
-install curl eject udisks2
+echo
+echo "${bold}INSTALLATION DE LOGICIELS HORS APT.${reset}"
+echo
 
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/devmatteini/dra/refs/heads/main/install.sh | bash -s -- --to ~/bin/
-chmod +x ~/bin/dra
+echo
+echo "${bold}Installation de dra.${reset}"
+echo
+
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/devmatteini/dra/refs/heads/main/install.sh | bash -s
+chmod +x ./dra
+sudo mv --force ./dra /usr/local/bin/
+
+if [ "$SHELL" = 'bash' ]; then
+  dra completion bash >|./dra
+  sudo mv --force ./dra /usr/local/share/bash-completion/completions
+fi
+
+echo
+echo "${bold}Installation de bashmount.${reset}"
+echo
 
 curl --remote-name https://raw.githubusercontent.com/jamielinux/bashmount/master/bashmount
 chmod +x ./bashmount
@@ -60,282 +331,27 @@ sudo mv --force ./bashmount /usr/local/bin/
 curl --remote-name https://raw.githubusercontent.com/jamielinux/bashmount/refs/heads/master/bashmount.1
 sudo mv --force ./bashmount.1 /usr/local/share/man/man1/
 
-pkgs="\
-alacritty
-arc
-arj
-atool
-bash-completion
-bat
-bc
-bfs
-blueman
-bluez
-bluez-tools
-brightnessctl
-bsdutils
-bzip2
-calibre
-cargo
-catdoc
-chafa
-chromium
-chromium-l10n
-clinfo
-cliphist
-cmake
-colordiff
-colortest-python
-cpio
-curl
-dbus
-dconf-editor
-dex
-dictionaries-common
-diffutils
-diskus
-dmidecode
-docx2txt
-dra
-du-dust
-duf
-dust
-e2fsprogs
-exfatprogs
-exif
-eza
-fastfetch
-fd-find
-fdisk
-ffmpegthumbnailer
-firefox-esr
-firefox-esr-l10n-fr
-foot
-fzf
-galternatives
-gcal
-glxinfo
-gnome-characters
-gnome-epub-thumbnailer
-golang
-gparted
-grim
-gucharmap
-gvfs
-gvfs-backends
-gvfs-fuse
-gzip
-htop
-hunspell
-hyperfine
-hyphen-fr
-imagemagick
-info
-inxi
-iso-info
-jekyll
-jq
-kanshi
-keepassxc
-kid3-qt
-kitty
-kitty-terminfo
-labwc
-lf
-libcpanel-json-xs-perl
-libdvd-pkg
-libfuse2t64
-libglib2.0-bin
-libnotify-bin
-libnotify-dev
-libreoffice-calc
-libreoffice-grammalecte
-libreoffice-help-fr
-libreoffice-java
-libreoffice-l10n-fr
-libreoffice-qt6
-libreoffice-writer
-libxml-dumper-perl
-localepurge
-lxpolkit
-lzip
-lzop
-mako-notifier
-mediainfo
-meson
-micro
-mintstick
-moreutils
-mousepad
-mpv
-neovim
-network-manager
-nomarch
-odt2txt
-pandoc
-parallel
-pdfarranger
-plocate
-poppler-utils
-qbittorrent
-qimgv
-qt5ct
-qt6ct
-rar
-rclone
-regionset
-rfkill
-ripgrep
-rpm
-rsync
-ruby-jekyll-paginate
-ruby-jekyll-sitemap
-screenruler
-sd
-sensible-utils
-shellcheck
-shfmt
-slurp
-swayidle
-tar
-tealdeer
-thunar
-thunar-archive-plugin
-thunar-gtkhash
-thunar-media-tags-plugin
-thunar-volman
-thunderbird
-thunderbird-l10n-fr
-timeshift
-transmission-cli
-trash-cli
-tree
-unace
-unalz
-unrar
-upower
-vainfo
-vlc
-vlc-plugin-pipewire
-vulkan-tools
-wayland-utils
-wev
-wget
-wl-clipboard
-wlopm
-wlr-randr
-wlrctl
-wmctrl
-xz-utils
-yt-dlp
-zathura"
-# abcde
-# archivemount
-# atril
-# catfish
-# featherpad
-# network-manager-gnome
-# numlockx
-# parted
-# partitionmanager
-# pavucontrol-qt
-# pavucontrol-qt-l10n
-# qpdf
-# quodlibet
-# sxhkd
-# synaptic
-# syncthing
-# transmission-qt
-# xarchiver
-# xcalib
-# xcape
-# xclip
-# xdotool
-# xfce4
-# xfce4-appfinder
-# xfce4-notifyd
-# xfce4-panel
-# xfce4-power-manager
-# xfce4-pulseaudio-plugin
-# xfce4-screenshooter
-# xfce4-session
-# xfce4-settings
-# xfce4-terminal
-# xfce4-xkb-plugin
-# xfconf
-# xfwm4
-
-install "$pkgs"
-
-# Enable and start timesync service
-# sudo systemctl enable --now systemd-timesyncd
-
-# Enable and start bluetooth service
-# sudo systemctl enable --now bluetooth
-
-# Enable and start polkitd
-# sudo systemctl enable --now polkitd
-
-# Enable and start rtkit
-# sudo systemctl enable --now rtkit
-
-# Set up NetworkManager
-# sudo systemctl stop wpa_supplicant
-# sudo systemctl disable --now wpa_supplicant
-
-# sudo systemctl disable --now systemd-networkd
-# sudo systemctl mask systemd-networkd
-
-# sudo systemctl enable --now dbus
-# sudo systemctl enable --now NetworkManager
-
-# Clone and set up dotfiles
-# git clone https://github.com/speyll/dotfiles "$HOME/dotfiles"
-# cp -r "$HOME/dotfiles/."* "$HOME/"
-# rm -rf "$HOME/dotfiles"
-# chmod -R +X "$HOME/.local/bin" "$HOME/.local/share/applications" "$HOME/.config/autostart/"
-# chmod +x "$HOME/.config/yambar/sway-switch-keyboard.sh" "$HOME/.config/yambar/xkb-layout.sh" "$HOME/.config/autostart/*" "$HOME/.local/bin/*"
-# ln -s "$HOME/.config/mimeapps.list" "$HOME/.local/share/applications/"
-
-# Add user to sudo group for sudo access
-# echo "%sudo ALL=(ALL:ALL) NOPASSWD: /usr/bin/halt, /usr/bin/poweroff, /usr/bin/reboot, /usr/bin/shutdown, /usr/bin/zzz, /usr/bin/ZZZ" | sudo tee -a /etc/sudoers.d/wheel
-
-# Créaction du mot de passe du compte root.
-sudo passwd root
-
-# Configuration de la console.
-sudo dpkg-reconfigure console-setup
-
-sudo sed --in-place 's/GRUB_TIMEOUT=./GRUB_TIMEOUT=2/' "/etc/default/grub"
-sudo update-grub
-
-sudo ln -s "/home/franck/.config/xkb/symbols/custom" "/usr/share/X11/xkb/rules/"
-
-ln -s "/home/franck/.config/lf/marks" "/home/franck/.local/share/lf/"
-
-sudo ln -s /usr/bin/batcat /usr/local/bin/bat
-sudo ln -s /usr/bin/fdfind /usr/local/bin/fd
-
-# Rendre exécutable le script de nettoyage des images affichées par lf dans kitty.
-chmod +x "/home/franck/.config/lf/lf_kitty_clean"
-
-# Après avoir installé
-sudo dpkg-reconfigure libdvd-pkg
-
-if [ "$SHELL" = 'bash' ]; then
-  dra completion bash >|./dra
-  sudo mv --force ./dra /usr/local/share/bash-completion/completions
-fi
+echo
+echo "${bold}Installation de pastel.${reset}"
+echo
 
 dra download --select "pastel_{tag}_amd64.deb" sharkdp/pastel
 install ./pastel*.deb
 rm ./pastel*.deb
+
+echo
+echo "${bold}Installation de uni.${reset}"
+echo
 
 dra download --select "uni-v{tag}-linux-amd64.gz" arp242/uni
 atool --extract uni*.gz
 rm --force uni*.gz
 sudo mv --force uni-v*-linux-amd64 /usr/local/bin/uni
 sudo chmod +x /usr/local/bin/uni
+
+echo
+echo "${bold}Installation de ouch.${reset}"
+echo
 
 dra download --select "ouch-x86_64-unknown-linux-gnu.tar.gz" ouch-org/ouch
 atool --extract ouch-x86_64-unknown-linux-gnu.tar.gz
@@ -344,11 +360,19 @@ sudo chmod +x /usr/local/bin/ouch
 sudo mv --force ./ouch-x86_64-unknown-linux-gnu/completions/ouch.bash /usr/local/share/bash-completion/completions/
 sudo mv --force ./ouch-x86_64-unknown-linux-gnu/man/* /usr/local/share/man/man1/
 
+echo
+echo "${bold}Installation de moar.${reset}"
+echo
+
 dra download --select "moar-v{tag}-linux-amd64" walles/moar
 sudo mv --force moar-*-*-* /usr/local/bin/moar
 sudo chmod +x /usr/local/bin/moar
 curl --remote-name https://raw.githubusercontent.com/walles/moar/refs/heads/master/moar.1
 sudo mv --force ./moar.1 /usr/local/share/man/man1/
+
+echo
+echo "${bold}Installation de fend.${reset}"
+echo
 
 dra download --select "fend-{tag}-linux-x86_64-gnu.zip" printfn/fend
 atool --extract fend-*-linux-x86_64-gnu.zip
@@ -356,6 +380,10 @@ sudo mv --force fend /usr/local/bin/
 sudo chmod +x /usr/local/bin/fend
 dra download --select "fend.1" printfn/fend
 sudo mv --force ./fend.1 /usr/local/share/man/man1/
+
+echo
+echo "${bold}Installation de diskus.${reset}"
+echo
 
 dra download --select "diskus_{tag}_amd64.deb" sharkdp/diskus
 install ./diskus*.deb
@@ -365,6 +393,10 @@ sudo rm ./diskus*.deb
 # dra download --select "flacon-{tag}-x86_64.AppImage" flacon/flacon
 # sudo mv flacon-*-*.AppImage /usr/local/bin/
 # sudo chmod +x /usr/local/bin/flacon-*-*.AppImage
+
+echo
+echo "${bold}Installation de advcpmv.${reset}"
+echo
 
 cd "$HOME/Projets" || exit 1
 
@@ -379,7 +411,9 @@ sudo mv --force "$HOME"/Projets/advcpmv/advcp /usr/local/bin/cpg
 sudo mv --force "$HOME"/Projets/advcpmv/advmv /usr/local/bin/mvg
 sudo chmod +x /usr/local/bin/cpg /usr/local/bin/mvg
 
-echo "Installation de massren."
+echo
+echo "${bold}Installation de massren.${reset}"
+echo
 
 if ! command -v go >/dev/null; then
   install golang
@@ -387,6 +421,10 @@ fi
 
 go install github.com/laurent22/massren@latest
 sudo cp --force "$GOPATH/bin/massren" /usr/local/bin/massren
+
+echo
+echo "${bold}Installation de wl-gammarelay-rs.${reset}"
+echo
 
 if ! command -v cargo >/dev/null; then
   echo "Installation de cargo"
@@ -397,9 +435,17 @@ echo "Installation de wl-gammarelay-rs"
 cargo install wl-gammarelay-rs --locked
 sudo cp --force "$CARGO_HOME/bin/wl-gammarelay-rs" /usr/local/bin
 
+echo
+echo "${bold}Installation de wlinhibit.${reset}"
+echo
+
 dra download --select "x86_64-unknown-linux-gnu" 0x5a4/wlinhibit
 sudo mv --force ./"x86_64-unknown-linux-gnu" /usr/local/bin/wlinhibit
 sudo chmod +x /usr/local/bin/wlinhibit
+
+echo
+echo "${bold}Installation de VSCodium.${reset}"
+echo
 
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
   | gpg --dearmor \
@@ -409,6 +455,10 @@ echo 'deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyri
   | sudo tee /etc/apt/sources.list.d/vscodium.list
 
 sudo apt update && sudo apt install codium
+
+echo
+echo "${bold}Installation de batsignal.${reset}"
+echo
 
 cd "$HOME/Projets" || exit 1
 
@@ -422,27 +472,67 @@ fi
 
 make && sudo make install
 
-# Correction des permissions des répertoires du système.
+echo
+echo "${bold}Installation de timemachine.${reset}"
+echo
+
+curl --remote-name https://raw.githubusercontent.com/cytopia/linux-timemachine/refs/heads/master/timemachine
+chmod +x ./timemachine
+sudo mv --force ./timemachine /usr/local/bin/
+
+echo
+echo "${bold}Installation de datedirclean.sh.${reset}"
+echo
+
+curl --remote-name https://raw.githubusercontent.com/meersjo/toolkit/refs/heads/master/various/datedirclean.sh
+chmod +x ./datedirclean.sh
+sudo mv --force ./datedirclean.sh /usr/local/bin/
+
+echo
+echo "${bold}Correction des permissions des répertoires du système.${reset}"
+echo
+
 sudo chmod 755 /usr/local/bin/*
 sudo chown root:root /usr/local/bin/*
-# à compiler
-# hyprpicker
 
-update-alternatives --config editor
-sudo update-alternatives --config editor
+echo
+echo "${bold}Configuration de « /etc/systemd/logind.conf ».${reset}"
+echo
 
 sudo sed --in-place 's/^#HandlePowerKey=.*/HandlePowerKey=ignore/' /etc/systemd/logind.conf
 sudo sed --in-place 's/^#IdleAction=.*/IdleAction=suspend/' /etc/systemd/logind.conf
 sudo sed --in-place 's/^#IdleActionSec=.*/IdleActionSec=4min/' /etc/systemd/logind.conf
 
-## désactiver certains services inutiles (il peut aussi être nécessaires de les masquer, pour empêcher que d'autres processus ne les lancent)
-systemctl --user disable gvfs-goa-volume-monitor.service
-systemctl --user mask gvfs-goa-volume-monitor.service
-sudo systemctl disable ModemManager.service
-sudo systemctl disable cups.service
-sudo systemctl disable cups-browsed.service
-sudo systemctl disable accounts-daemon
-sudo systemctl mask accounts-daemon
+echo
+echo "${bold}Configuration de grub.${reset}"
+echo
+
+sudo sed --in-place 's/GRUB_TIMEOUT=./GRUB_TIMEOUT=2/' "/etc/default/grub"
+sudo update-grub
+
+echo
+echo "${bold}Établissement de liens symboliques de batcat et fdfind vers bat et fd.${reset}"
+echo
+
+sudo ln -s /usr/bin/batcat /usr/local/bin/bat
+sudo ln -s /usr/bin/fdfind /usr/local/bin/fd
+
+echo
+echo "${bold}Création du mot de passe du compte root.${reset}"
+echo
+
+sudo passwd root
+
+echo '######################################'
+echo "${bold}# CONFIGURATION DES DONNÉES PRIVÉES. #${reset}"
+echo '######################################'
+
+sudo ln -s "/home/franck/.config/xkb/symbols/custom" "/usr/share/X11/xkb/rules/"
+
+ln -s "/home/franck/.config/lf/marks" "/home/franck/.local/share/lf/"
+
+# Rendre exécutable le script de nettoyage des images affichées par lf dans kitty.
+chmod +x "/home/franck/.config/lf/lf_kitty_clean"
 
 ## Configurer le clavier (y compris celui de la console?).
 sudo dpkg-reconfigure keyboard-configuration
@@ -457,10 +547,17 @@ XKBOPTIONS="compose:ins,nbsp:level3n,kpdl:comma"
 BACKSPACE="guess"
 XKBVARIANT="fg_invert_home_end_with_pageup_pagedown,"' >|/etc/default/keyboard
 
-curl --remote-name https://raw.githubusercontent.com/cytopia/linux-timemachine/refs/heads/master/timemachine
-chmod +x ./timemachine
-sudo mv --force ./timemachine /usr/local/bin/
+update-alternatives --config editor
+sudo update-alternatives --config editor
 
-curl --remote-name https://raw.githubusercontent.com/meersjo/toolkit/refs/heads/master/various/datedirclean.sh
-chmod +x ./datedirclean.sh
-sudo mv --force ./datedirclean.sh /usr/local/bin/
+# À COMPILER
+# hyprpicker
+
+## désactiver certains services inutiles (il peut aussi être nécessaires de les masquer, pour empêcher que d'autres processus ne les lancent)
+# systemctl --user disable gvfs-goa-volume-monitor.service
+# systemctl --user mask gvfs-goa-volume-monitor.service
+# sudo systemctl disable ModemManager.service
+# sudo systemctl disable cups.service
+# sudo systemctl disable cups-browsed.service
+# sudo systemctl disable accounts-daemon
+# sudo systemctl mask accounts-daemon
